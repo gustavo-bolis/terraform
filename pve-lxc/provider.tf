454934b2cb3be6852e41845ff0a857da44afd54e
Gustavo_Bolis <gustavo.bolis@ixcsoft.com.br>
2023-03-10 15:02:18 -0300

    initial commit

--- pve-lxc/provider.tf
+++ pve-lxc/provider.tf
@@ -1,20 +1,26 @@
 variable "proxmox_url" {
   type      = string
   sensitive = true
+
 }
 
 variable "proxmox_token_id" {
   type      = string
   sensitive = true
+
 }
 
 variable "proxmox_token_secret" {
   type      = string
   sensitive = true
+
 }
 
 provider "proxmox" {
-  pm_api_url          = var.proxmox_url
-  pm_api_token_id     = var.proxmox_token_id
+  pm_api_url = var.proxmox_url
+
+  pm_api_token_id = var.proxmox_token_id
+
   pm_api_token_secret = var.proxmox_token_secret
+
 }
